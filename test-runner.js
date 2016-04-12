affb36031f9ed3bbdc022de7a8e4a8c5ff4d8316
Andrey Panteleyev <insonifi@gmail.com>
2016-04-12 09:17:20 +0300

    No need for alpha anymore

--- test-runner.js
+++ test-runner.js
@@ -26,7 +26,6 @@ const TOLERANCE = nconf.get('tolerance');
 const CPU_THRESHOLD = nconf.get('cpu-threshold');
 const CPU_MIN_SAMPLES = nconf.get('cpu-samples');
 const FPS_THRESHOLD = nconf.get('fps-threshold');
-const A = nconf.get('alpha');
 const stopOnExit = nconf.get('stop');
 const INIT_COUNT = nconf.get('cams');
 const VALIDATE_COUNT = nconf.get('validate');
