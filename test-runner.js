7c9495df419ae0fe077f11101437e71aa1e8c863
Andrey Panteleyev <insonifi@gmail.com>
2016-04-12 09:22:43 +0300

    Make sure there is new line on error output

--- test-runner.js
+++ test-runner.js
@@ -253,10 +253,10 @@ process.on('exit', () => {
   } 
 });
 process.on('uncaughtException', (err) => {
-  console.error(`${progressTime()}\nCaught exception:`, err);
+  console.error(`\n${progressTime()}\nCaught exception:`, err);
 });
 process.on('unhandledRejection', (reason, p) => {
-  console.error(`${progressTime()}\nUnhandled Rejection at: Promise `, p, 'reason: ', reason.stack);
+  console.error(`\n${progressTime()}\nUnhandled Rejection at: Promise `, p, 'reason: ', reason.stack);
 });
 
 /* Prepare video stream URI */
