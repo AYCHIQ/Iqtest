0affa33d7fabe3e65855ee5ac4910ba97efd557b
Andrey Panteleyev <insonifi@gmail.com>
2016-09-21 16:34:09 +0300

    toDoubleDigits truncates hours if it's more than 99

--- timing.js
+++ timing.js
@@ -21,7 +21,7 @@ class Timing {
   }
   getTimeString(n) {
     const t = isFinite(n) ? n : 0;
-    const hrs = this.toDoubleDigit(t / HOURS);
+    const hrs = (t / HOURS);
     const m = t % HOURS;
     const min = this.toDoubleDigit(m / MINUTES);
     const s = m % MINUTES;
